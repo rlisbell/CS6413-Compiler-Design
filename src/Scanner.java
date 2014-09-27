453bd31ada500eedfb00458ed34538855886bd77
Bobboau <Bobboau@gmail.com>
2014-09-27 18:24:08 -0400

    fixed handling of end of file

--- src/Scanner.java
+++ src/Scanner.java
@@ -1,6 +1,7 @@
 import java.io.*;
 
 public class Scanner {
+	
 	/**
 	 * input giving us the source code
 	 */
@@ -17,6 +18,11 @@ public class Scanner {
 	private char last_char;
 	
 	/**
+	 * we have read past the end of the file
+	 */
+	private boolean eof;
+	
+	/**
 	 * basic constructor, specifies the source code input stream
 	 * @param _source
 	 */
@@ -24,14 +30,18 @@ public class Scanner {
 		source = _source;
 		line_number = 0;
 		last_char = '\0';
+		eof = false;
 	}
 	
 	/**
 	 * Begins fetching next token
-	 * @return
+	 * @return Token the next one in the file, or null if there were none
 	 * @throws IOException
 	 */
 	public Token getNextToken() throws IOException{
+		if(eof){
+			return null;
+		}
 		skipNonlexeme();
 		int starting_line = line_number;
 		String lexeme = getLexeme();
@@ -47,10 +57,12 @@ public class Scanner {
 		StringBuilder lexeme = new StringBuilder();
 		char new_char;
 		
-		while(isLexemeChar(new_char = getChar())){
+		while(isLexemeChar(new_char = getChar()) && !eof){
 			lexeme.append(new_char);
 		}
-		backOut(new_char);
+		if(!eof){
+			backOut(new_char);
+		}
 		return lexeme.toString();
 	}
 
@@ -98,11 +110,9 @@ public class Scanner {
 		
 		if(new_char == '{'){
 			skip = true;
-			do{
-				//TODO: Throw error on {
-			}while(getChar() != '}');
+			while(getChar() != '}' && !eof);//TODO: Throw error on {
 		}
-		else{
+		else if(!eof){
 			backOut(new_char);
 		}
 		return skip;
@@ -116,10 +126,12 @@ public class Scanner {
 	private boolean skipWhiteSpace() throws IOException{
 		boolean skip = false;
 		char new_char;
-		while(isWhiteSpace(new_char = getChar())){
+		while(isWhiteSpace(new_char = getChar()) && !eof){
 			skip = true;
 		}
-		backOut(new_char);
+		if(!eof){
+			backOut(new_char);
+		}
 		return skip;
 	}
 	
@@ -131,8 +143,10 @@ public class Scanner {
 	private boolean skipNewlines() throws IOException{
 		boolean skip = false;
 		char new_char;
-		while(isNewline(new_char = getChar()));
-		backOut(new_char);
+		while(isNewline(new_char = getChar()) && !eof);
+		if(!eof){
+			backOut(new_char);
+		}
 		return skip;
 	}
 
@@ -162,17 +176,30 @@ public class Scanner {
 	 * @throws IOException
 	 */
 	private char getChar() throws IOException {
-		char next_char;
-		next_char = (char) source.read();
+		int next_char;
+		
+		next_char = source.read();
+		
+		//we check for end of line
+		if(next_char == -1){
+			eof = true;
+		}
+		
+		//now we check for windows' messed up newlines
 		if(next_char == '\n' && last_char == '\r'){
 			//windows newlines are screwy, \r\n should be treated as a single character
 			next_char = (char) source.read();
 		}
-		last_char = next_char;
-		if(isNewline(next_char)){
+		
+		//new we record the old char so next time we can find out if it's a windows newline
+		last_char = (char)next_char;
+		
+		//if we got any form of newline increment the line count
+		if(isNewline(last_char)){
 			line_number++;
 		}
-		return next_char;
+		
+		return last_char;
 	}
 
 	/**
@@ -181,6 +208,8 @@ public class Scanner {
 	 */
 	private void backOut(char c) throws IOException{
 		source.unread((int) c);
+		
+		//if we are pushing back a newline then we need to move the line count back
 		if(isNewline(c)){
 			line_number--;
 		}
