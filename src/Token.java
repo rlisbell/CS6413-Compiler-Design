d0f97f6f56f70278dd742360a5ad364c0d9b38c4
Bobboau <bobboau@gmail.com>
2014-10-04 14:17:14 -0400

    reworked Tokens to be simpler and to have a lot of their work pushed off to the symbol table

--- src/Token.java
+++ src/Token.java
@@ -13,81 +13,17 @@ public class Token {
 	 * pattern that matches parentheses and brackets
 	 * longer patterns must be listed first
 	 */
-	static final Pattern TOKEN_PAREN_PATTERN = Pattern.compile("^((\\()|(\\))|(\\[)|(\\]))", Pattern.CASE_INSENSITIVE);
-	
-	/**
-	 * pattern that matches :;.,
-	 * longer patterns must be listed first
-	 */
-	static final Pattern TOKEN_PUNCTUATION_PATTERN = Pattern.compile("^((\\,)|(\\.)|(\\;)|(\\:))", Pattern.CASE_INSENSITIVE);
-	
-	/**
-	 * pattern that matches operator type tokens
-	 * longer patterns must be listed first
-	 */
-	static final Pattern TOKEN_OPERATOR_PATTERN = Pattern.compile("^((<>)|(<=)|(>=)|(:=)|(DIV)|(MOD)|(OR)|(AND)|(,)|(\\()|(\\))|(;)|([-+/*=<>]))", Pattern.CASE_INSENSITIVE);
-	
-	/**
-	 * pattern that matches if/else
-	 * longer patterns must be listed first
-	 */
-	static final Pattern TOKEN_IF_ELSE_PATTERN = Pattern.compile("^((IF)|(THEN)|(ELSE))", Pattern.CASE_INSENSITIVE);
-	
-	/**
-	 * pattern that matches loop structures
-	 * longer patterns must be listed first
-	 */
-	static final Pattern TOKEN_LOOP_PATTERN = Pattern.compile("^((WHILE)|(DO))", Pattern.CASE_INSENSITIVE);
-	
-	/**
-	 * pattern that matches variable structures
-	 * longer patterns must be listed first
-	 */
-	static final Pattern TOKEN_VARIABLE_PATTERN = Pattern.compile("^((FUNCTION)|(PROCEDURE))", Pattern.CASE_INSENSITIVE);
-	
-	/**
-	 * pattern that matches method structures
-	 * longer patterns must be listed first
-	 */
-	static final Pattern TOKEN_METHOD_PATTERN = Pattern.compile("^VAR", Pattern.CASE_INSENSITIVE);
-	
-	/**
-	 * pattern that matches array structures
-	 * longer patterns must be listed first
-	 */
-	static final Pattern TOKEN_ARRAY_PATTERN = Pattern.compile("^((ARRAY)|(OF))", Pattern.CASE_INSENSITIVE);
-	
-	/**
-	 * pattern that matches block structures
-	 * longer patterns must be listed first
-	 */
-	static final Pattern TOKEN_BLOCK_PATTERN = Pattern.compile("^((BEGIN)|(END))", Pattern.CASE_INSENSITIVE);
-	
-	/**
-	 * pattern that matches basic type structures
-	 * longer patterns must be listed first
-	 */
-	static final Pattern TOKEN_TYPE_PATTERN = Pattern.compile("^((INTEGER)|(REAL))", Pattern.CASE_INSENSITIVE);
-	
-	/**
-	 * pattern that matches high level program structures
-	 * longer patterns must be listed first
-	 */
-	static final Pattern TOKEN_PROGRAM_PATTERN = Pattern.compile("^((PROGRAM)|(\\.))", Pattern.CASE_INSENSITIVE);
-	
-	
-	/**
-	 * pattern that matches loop structures
-	 * longer patterns must be listed first
-	 */
-	static final Pattern TOKEN_IDENTIFIER_PATTERN = Pattern.compile("^\\w[\\w\\d]*", Pattern.CASE_INSENSITIVE);
-	
-	/**
-	 * pattern that matches number type tokens 
-	 * \\d+  (\\.  \\d+)?   (E [+-]?  \\d+)?
-	 * 1+ digits, optional . and 1+ digits, optional (E optional +/- and 1+ digits)
-	 */
-	static final Pattern TOKEN_NUMBER_PATTERN = Pattern.compile("^\\d+(\\.\\d+)?(E[+-]?\\d+)?", Pattern.CASE_INSENSITIVE);
+	static final Pattern TOKEN_LEXEME_PATTERN = Pattern.compile("^("+
+			"(\\()|(\\))|(\\[)|(\\])"
+			+"|"+
+			"(\\,)|(\\.)|(\\;)|(\\:)"
+			+"|"+
+			"(<>)|(<=)|(>=)|(:=)|(DIV)|(MOD)|(OR)|(AND)|(,)|(\\()|(\\))|(;)|([-+/*=<>])"
+			+"|"+
+			"([a-z]\\w*)"
+			+"|"+
+			"(\\d+(\\.\\d+)?(E[+-]?\\d+)?)"
+		+")", Pattern.CASE_INSENSITIVE);
 	
 	/**
 	 * enum that specifies the different options for what sort of token we might have 
@@ -174,31 +110,15 @@ public class Token {
 	 * @return Token
 	 */
 	public static Token makeToken(String lexeme_block, int line_number){
-		Matcher operator_matcher = TOKEN_OPERATOR_PATTERN.matcher(lexeme_block);
-		Matcher number_matcher = TOKEN_NUMBER_PATTERN.matcher(lexeme_block);
-		Matcher word_matcher = TOKEN_WORD_PATTERN.matcher(lexeme_block);
+		Matcher lexeme_matcher = TOKEN_LEXEME_PATTERN.matcher(lexeme_block);
 		
 		String lexeme = "";
 		Type type;
 		
-		//order is important!
-		/*checking of keywords should happen here*/
-		/*we are not dealing with keywords yet though*/
-		if(lexeme_block.equals(".")){
-			type = Token.Type.EOF;
-			lexeme = lexeme_block;
-		}
-		else if(operator_matcher.find()){
-			type = Token.Type.OPERATOR;
-			lexeme = operator_matcher.group(0);
-		}
-		else if(number_matcher.find()){
-			type = Token.Type.NUMBER;
-			lexeme = number_matcher.group(0);
-		}
-		else if(word_matcher.find()){
-			type = Token.Type.WORD;
-			lexeme = word_matcher.group(0);
+		if(lexeme_matcher.find()){
+			/*TODO:use symbol table*/
+			type = Token.Type.ERROR;
+			lexeme = lexeme_matcher.group(0);
 		}
 		else{
 			type = Token.Type.ERROR;
